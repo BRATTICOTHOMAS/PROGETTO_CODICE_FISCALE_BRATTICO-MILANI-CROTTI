dcddc6e652b94b60f3e7db336091c6e568502792
crottialessio <crotti.alessio.09@itisdalmine.edu.it>
2026-02-27 15:55:26 +0100

    secondo commit

--- libreria_crotti.py
+++ libreria_crotti.py
@@ -1,24 +1,21 @@
-import datetime
+
 def chiediSesso():
     corretto=False
     while not corretto:
         sesso=input("inserisci il sesso m/f")
         
-        if sesso.lower()=="m":
-            return "m"
+        if sesso.upper()=="M":
+            return "M"
             
-        elif sesso.lower()=="f":
-            return"f"
+        elif sesso.upper()=="F":
+            return"F"
         else:print("inserimento del sesso errato")
             
 
 sesso=chiediSesso()
-print(sesso)
-
-date=datetime.datetime(1999,3,11) #!DA TOGLIERE
 def calcolaCodiceGiorno(date,sesso):
     g=date.day
-    if sesso=="m":
+    if sesso=="M":
         if g<10:
            # print("ciao")
            g=str(g)
@@ -28,8 +25,9 @@ def calcolaCodiceGiorno(date,sesso):
         else:
             g=str(g)
             #print(g)
+            return g
            
-    elif sesso=="f":
+    elif sesso=="F":
         g+=40
         g=str(g)
         #print(g)
@@ -38,3 +36,20 @@ def calcolaCodiceGiorno(date,sesso):
 
 codice_g=calcolaCodiceGiorno(date,sesso)
 print(codice_g)
+
+def chiedinome():
+    corretto=False
+    while not corretto:
+        try:
+            nome_ins=input("inserisci il nome (no numeri e min 2 lettere)")
+            nome_ins=rimuovi_spazi(nome_ins)
+            nome_ins=rimuovilettereaccentate(nome_ins)
+            if nome_ins.isalpha()==True:
+                corretto=True
+        
+                return nome_ins
+            else:print("puoi inserire solo lettere")
+        
+                #print("ciao")
+        except:print("ERRORE NEL FORMATO")
+
